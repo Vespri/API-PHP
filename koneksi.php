b7166d645c102fc89a84f4ab0d33bdff28def6cc
Vespri <kresna.vespri21@gmail.com>
2022-01-04 11:40:28 +0700

    Add check connection on koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -5,5 +5,7 @@ $password	= "";
 $database	= "komputer_polsri";
 
 $db_connect = new mysqli($host, $user, $password, $database);
-
+if($db_connect->connect_error){
+    die("Koneksi Gagal: ". $db_connect->connect_error);
+}
 ?>
